a77dc934016fd8f841c69c910d9cddd307294ea6
Ajay Chavan <ajayc9838@gmail.com>
2022-11-22 18:06:39 +0530

    completed

--- wat-a-pizza-solution.js
+++ wat-a-pizza-solution.js
@@ -77,28 +77,99 @@ const order = {
 
 //Write function to filter menu items by Category and sort them alphabetically
 function listByCategory(menu, categoryName){
-    let filterResult = menu.filter(item => item.category == 'Beverages').map(item =>{
-        return "Category : "+categoryName+" Name : "+item.name+" Price : "+item.price
-    })
+    let filterResult = menu.filter(item => item.category == categoryName)
     return filterResult;
-}
+    }
+ let starterCat = listByCategory(menu,"Starters");
+ let mainCat = listByCategory(menu,"Main Meal");
+ let bevareageCat = listByCategory(menu,"Beverages")
 
-console.log(listByCategory(menu,"Starters"))
+console.log(starterCat)
+console.log(mainCat)
+
+function sorting(cat){
+return cat.sort(function(x,y){
+    if(x.name > y.name){
+        return 1;
+    }
+    else if(x.name < y.name){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+});
+}
+console.log("-------------------------------")
+let sortedStarterCat = sorting(starterCat);
+console.log(sortedStarterCat);
+
+let sortedMainCat = sorting(mainCat);
+console.log(sortedMainCat)
+
+let sortedBevarages = sorting(bevareageCat);
+console.log(sortedBevarages);
+
 
 //Write function to calculate the amount of each ordered item
-function calculateAmount(menu, order){
-    let mapAmount = order.filter(item => item.name == "")
+function calculateAmount(menu,order){
+    let calOrder = order.items.map(item => {
+        return{ name : item.name,
+                price : item.price,
+                quantity : item.quantity,
+                category: menu.filter(menuItem => item.name == menuItem.name)[0].category,
+                amount: item.price * item.quantity
+        }
+    })
+    return calOrder;
 }
 
-//Write function to calculate the subtotal amount for the main meal ordered
+let price = calculateAmount(menu,order);
+console.log(price)
 
+//Write function to calculate the subtotal amount for the main meal ordered
 //Write function to find the totalAmount of the order based on the calculated mainMealAmount value
+function calculateMainMealAmount(mainCat){
+    let sumOrder = mainCat.reduce(function(x,y){
+        return x+y.price;
+    },0);
+    return sumOrder
+}
+
+let mainMealAmount = calculateMainMealAmount(mainCat);
+console.log("Total Main Meal Amount is "+mainMealAmount);
+
 
 //Write function to calculate the final amount after discount
+discountApplied = 0;
+finalPrice = 0;
+function calculatedFinalAmount(totalAmount, discount){
+    if(totalAmount > 50){
+        discountApplied = (totalAmount *5)/100;
+        return finalPrice = totalAmount - discountApplied;
+    }else{
+        return totalAmount;
+    }
+}
+
+let appliedDiscountPrice = calculatedFinalAmount(mainMealAmount,5);
+console.log("Final Bill Amount is "+appliedDiscountPrice);
 
 //Write function to display the total amount 
+function displayTotalAmount(){
+    return calculateMainMealAmount(mainCat)
+}
+
+let d = displayTotalAmount();
+console.log("Total Amount payable: $"+d);
 
 //Write function to display the final amount
+function displayFinalAmount(){
+    return appliedDiscountPrice;
+}
+
+let f = displayFinalAmount();
+console.log("Final Amount payable after discount: $"+f)
 
 //Invoke  functions 
 
